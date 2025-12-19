07fd12b1bd18d93f72858f8af6689a62395d3a3e
Eric Miller <ermiller@alumni.cmu.edu>
2025-12-19 16:02:15 -0500

    update test dep + coverage plugin

--- src/test/java/gov/nih/nci/bento/controller/GraphQLControllerTest.java
+++ src/test/java/gov/nih/nci/bento/controller/GraphQLControllerTest.java
@@ -14,8 +14,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
